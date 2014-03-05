1ec206e97bda9a649da531686741476bb2197ad2
datalyze-solutions <development@datalyze-solutions.com>
2014-03-05 10:16:04 +0100

    unused original code removed

--- libs/remoteserver.py
+++ libs/remoteserver.py
@@ -58,71 +58,12 @@ class QgsRemoteCommandServer(QTcpServer):
             sockets.remove(sender)
             self.broadcast(sockets, commandByteStream)
 
-        # without loop
-        #if sender.bytesAvailable() > 0:
-            #stream = QDataStream(sender)
-            #stream.setVersion(QDataStream.Qt_4_2)
-            #if sender.nextBlockSize == 0:
-                #if sender.bytesAvailable() < SIZEOF_UINT32:
-                    #return
-                #sender.nextBlockSize = stream.readUInt32()
-            #if sender.bytesAvailable() < sender.nextBlockSize:
-                #return
-            #command = stream.readQVariant()
-            #sender.nextBlockSize = 0
-            #if self.sendMessageBackToSender:                
-                #self.broadcastCommands(self.connections, command)
-            #else:
-                #sockets = copy(self.connections)
-                #sockets.remove(sender)
-                #self.broadcastCommands(sockets, command)
-            #sender.nextBlockSize = 0
-
-        # original
-        #for s in self.connections:
-            #if s.bytesAvailable() > 0:
-                #stream = QDataStream(s)
-                #stream.setVersion(QDataStream.Qt_4_2)
-
-                #if s.nextBlockSize == 0:
-                    #if s.bytesAvailable() < SIZEOF_UINT32:
-                        #return
-                    #s.nextBlockSize = stream.readUInt32()
-                #if s.bytesAvailable() < s.nextBlockSize:
-                    #return
-
-                #command = stream.readQVariant()
-                #s.nextBlockSize = 0
-                #self.sendMessage(command, s.socketDescriptor())
-                #s.nextBlockSize = 0
-
-    #def sendMessage(self, command, socketId):        
-        #for s in self.connections:
-            #if s.socketDescriptor() == socketId:
-                #if self.sendMessageBackToSender:
-                    #self.sendAnswer(s, command)
-            #else:
-                #self.sendAnswer(s, command)
-
     def broadcast(self, sockets, commandByteStream):
         for socket in sockets:
             socket.write(commandByteStream)
-            #self.sendAnswer(socket, commandByteStream)
-
-    # old version
-    #def sendAnswer(self, socket, command):
-        #reply = QByteArray()
-        #stream = QDataStream(reply, QIODevice.WriteOnly)
-        #stream.setVersion(QDataStream.Qt_4_2)
-        #stream.writeUInt32(0)
-        #stream.writeQVariant(command)
-        #stream.device().seek(0)
-        #stream.writeUInt32(reply.size() - SIZEOF_UINT32)        
-        #socket.write(reply)
 
     def removeConnection(self):
         self.connections.remove(self.sender())
 
     def socketError(self):
         self.sender().disconnectFromHost()
-        #self.connections.remove(self.sender())
